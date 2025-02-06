28c5b61215df00bbf7fb72a4b0b9a05e91c15cfd
Akikudo <gregorysh2609@icloud.com>
2025-02-07 00:40:35 +0300

    недописанная версия

--- buttons.py
+++ buttons.py
@@ -2,15 +2,25 @@ import pygame
 
 
 class Button:
-    def __init__(self, x, y, width, height, text, image_path, hover_im_path=None):
+    def __init__(self, ev, name, x, y, width, height, text, image_path, hover_im_path=None, size=20):
         self.x = x
+        self.u = 0
+        self.namer = name
         self.y = y
+        self.size = size
         self.width = width
+        self.ev = ev
         self.height = height
         self.text = text
         self.image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(self.image, (width, height))
         self.hover_image = self.image
+        self.protfield = 0
+        self.reconstr = 0
+        self.doubledmg = 0
+        self.momentdmg = 0
+        self.init_param = 0
+
         if hover_im_path:
             self.hover_image = pygame.image.load(hover_im_path)
             self.hover_image = pygame.transform.scale(self.hover_image, (width, height))
@@ -23,8 +33,8 @@ class Button:
         else:
             current_img = self.image
         screen.blit(current_img, self.rect.topleft)
-        font = pygame.font.Font(None, 36)
-        text_surf = font.render(self.text, True, (255, 255, 255))
+        font = pygame.font.Font(None, self.size)
+        text_surf = font.render(self.text, True, (0, 0, 0))
         text_rect = text_surf.get_rect(center=self.rect.center)
         screen.blit(text_surf, text_rect)
 
@@ -34,3 +44,37 @@ class Button:
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
             pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
+
+    def button_event(self, num: int, nam: str):
+        if num == 1:
+            if nam == 'momentdmg_button' and self.u >= 1200:
+                self.momentdmg += 1
+                self.u -= 1200
+            elif nam == 'protfield_button' and self.u >= 500:
+                self.protfield += 1
+                self.u -= 500
+            elif nam == 'reconstr_button' and self.u >= 600:
+                self.reconstr += 1
+                self.u -= 500
+            elif nam == 'doubledmg_button' and self.u >= 1200:
+                self.doubledmg += 1
+                self.u -= 1200
+        elif num == 2:
+            if nam == 'lightgame_button':
+                self.init_param = {'rows': 9, 'cols': 10, 'mode_coeff': 0.5, 'HP': 0}
+            elif nam == 'mediumgame_button':
+                self.init_param = {'rows': 9, 'cols': 10, 'mode_coeff': 1, 'HP': 0}
+            elif nam == 'hardgame_button':
+                self.init_param = {'rows': 9, 'cols': 10, 'mode_coeff': 1.5, 'HP': 0}
+
+
+
+class Label:
+    def __init__(self, x, y, size=30, type=None):
+        self.x = x
+        self.y = y
+        self.font_type = pygame.font.Font(type, size)
+
+    def print_text_pygame(self, screen, text):
+        self.text = self.font_type.render(text, True, (0, 0, 0))
+        screen.blit(self.text, (self.x, self.y))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
+
 import pygame
-from buttons import Button
+
+from buttons import Button, Label
 
 pygame.init()
 
@@ -8,17 +10,93 @@ width, height = 1500, 700
 running = True
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('df')
-green_button = Button(width/2-(252/2), 100, 252, 211, '', 'kaspersky.png', 'mcaffee.png')
-
+a = dict()
+d = []
+protfield_button = Button(1, 'protfield_button', 100, 500, 280, 30, 'Купить защитное поле за 500 очков', 'img.png',
+                          'img_1.png')
+a[str(protfield_button)] = [protfield_button.x, protfield_button.y, protfield_button.width, protfield_button.height]
+d.append(protfield_button)
+reconstr_button = Button(1, 'reconstr_button', 100, 540, 280, 30, 'Купить реконструкцию за 600 очков', 'img.png',
+                         'img_1.png')
+a[str(reconstr_button)] = [reconstr_button.x, reconstr_button.y, reconstr_button.width, reconstr_button.height]
+d.append(reconstr_button)
+doubledmg_button = Button(1, 'doubledmg_button', 100, 580, 280, 30, 'Купить двойной урон за 1200 очков', 'img.png',
+                          'img_1.png')
+a[str(doubledmg_button)] = [doubledmg_button.x, doubledmg_button.y, doubledmg_button.width, doubledmg_button.height]
+d.append(doubledmg_button)
+momentdmg_button = Button(1, 'momentdmg_button', 100, 620, 280, 30, 'Купить моментальный урон за 1200 очков', 'img.png',
+                          'img_1.png')
+a[str(momentdmg_button)] = [momentdmg_button.x, momentdmg_button.y, momentdmg_button.width, momentdmg_button.height]
+d.append(momentdmg_button)
+lightgame_button = Button(2, 'lightgame_button', 100, 220, 280, 100, 'Режим для новичков', 'img.png', 'img_1.png')
+a[str(lightgame_button)] = [lightgame_button.x, lightgame_button.y, lightgame_button.width, lightgame_button.height]
+d.append(lightgame_button)
+mediumgame_button = Button(2, 'mediumgame_button', 500, 220, 280, 100, 'Режим для опытных', 'img.png', 'img_1.png')
+a[str(mediumgame_button)] = [mediumgame_button.x, mediumgame_button.y, mediumgame_button.width, mediumgame_button.height]
+d.append(mediumgame_button)
+hardgame_button = Button(2, 'hardgame_button', 900, 220, 280, 100, 'Лютый хардкор', 'img.png', 'img_1.png')
+a[str(hardgame_button)] = [hardgame_button.x, hardgame_button.y, hardgame_button.width, hardgame_button.height]
+d.append(hardgame_button)
+hack_spd_button = Button(3, 'hack_spd_button', 900, 500, 280, 30, 'Купить увеличение скорости взлома', 'img.png',
+                         'img_1.png')
+a[str(hack_spd_button)] = [hack_spd_button.x, hack_spd_button.y, hack_spd_button.width, hack_spd_button.height]
+d.append(hack_spd_button)
+noticecode_button = Button(3, 'noticecode_button', 900, 500, 280, 30, 'Купить незаметность шифровки', 'img.png',
+                           'img_1.png')
+a[str(noticecode_button)] = [noticecode_button.x, noticecode_button.y, noticecode_button.width, noticecode_button.height]
+d.append(noticecode_button)
+'''hack_spd_button = Button(3, 'hack_spd_button', 900, 500, 280, 30, 'Купить увеличение здоровья вируса', 'img.png',
+                         'img_1.png')
+a[hardgame_button] = [hardgame_button.x, hardgame_button.y, hardgame_button.width, hardgame_button.height]
+hack_spd_button = Button(3, 'hack_spd_button', 900, 500, 280, 30, 'Купить увеличение урона вируса', 'img.png',
+                         'img_1.png')
+a[hardgame_button] = [hardgame_button.x, hardgame_button.y, hardgame_button.width, hardgame_button.height]'''
+la_equip = Label(100, 440)
+la_protfield = Label(400, 500)
+la_reconstr = Label(400, 540)
+la_doubledmg = Label(400, 580)
+la_momentdmg = Label(400, 620)
+la_amountequip = Label(400, 440)
+la_points = Label(1300, 100)
+la_improvement = Label(900, 440)
 while running:
+    screen.fill('white')
     for event in pygame.event.get():
-        green_button.handle_event(event)
+        protfield_button.handle_event(event)
+        reconstr_button.handle_event(event)
+        doubledmg_button.handle_event(event)
+        momentdmg_button.handle_event(event)
+        hack_spd_button.handle_event(event)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.USEREVENT:
             pass
-    green_button.check_hover(pygame.mouse.get_pos())
-    green_button.draw_button(screen)
+            if event.Button.init_param != 0:
+                init_param = event.button.init_param
+    la_improvement.print_text_pygame(screen, f'улучшения')
+    la_points.print_text_pygame(screen, f'{protfield_button.u}')
+    la_protfield.print_text_pygame(screen, f'{protfield_button.protfield}')
+    la_reconstr.print_text_pygame(screen, f'{reconstr_button.reconstr}')
+    la_doubledmg.print_text_pygame(screen, f'{doubledmg_button.doubledmg}')
+    la_momentdmg.print_text_pygame(screen, f'{momentdmg_button.momentdmg}')
+    la_amountequip.print_text_pygame(screen, 'кол-во снаряжения')
+    la_equip.print_text_pygame(screen, 'снаряжение')
+    lightgame_button.check_hover(pygame.mouse.get_pos())
+    lightgame_button.draw_button(screen)
+    mediumgame_button.check_hover(pygame.mouse.get_pos())
+    mediumgame_button.draw_button(screen)
+    hardgame_button.check_hover(pygame.mouse.get_pos())
+    hardgame_button.draw_button(screen)
+    protfield_button.check_hover(pygame.mouse.get_pos())
+    protfield_button.draw_button(screen)
+    reconstr_button.check_hover(pygame.mouse.get_pos())
+    reconstr_button.draw_button(screen)
+    doubledmg_button.check_hover(pygame.mouse.get_pos())
+    doubledmg_button.draw_button(screen)
+    momentdmg_button.check_hover(pygame.mouse.get_pos())
+    momentdmg_button.draw_button(screen)
+    hack_spd_button.check_hover(pygame.mouse.get_pos())
+    hack_spd_button.draw_button(screen)
     pygame.display.flip()
 pygame.quit()
 sys.exit()
